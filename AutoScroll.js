07931bef1b0d644fa755c722e6a13f01431f8132
Fritz Lin <uxfritz@163.com>
2016-10-04 18:12:38 +0800

    fix handleContentChange repeated called on Android

--- AutoScroll.js
+++ AutoScroll.js
@@ -55,7 +55,11 @@ export default class AutoScroll extends Component {
   handleLayout (e) {
     this.scrollHeight = e.nativeEvent.layout.height
   }
+
   handleContentChange (w, h) {
+    // repeated called on Android
+    // should do diff
+    if (h === this.contentHeight) return
     this.contentHeight = h
 
     if (this.scrollHeight == null) {
